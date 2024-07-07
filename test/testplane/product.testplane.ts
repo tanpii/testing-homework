be9fd1db69cdc6d7bbd6f37fb06389f66e547e4e
tanyaporseva <tporseva@inbox.ru>
2024-07-08 00:00:36 +0300

    unit and testplane tests updated

--- test/testplane/product.testplane.ts
+++ test/testplane/product.testplane.ts
@@ -1,1 +1,18 @@
-пш
+import { bug } from "../bug";
+
+describe('', () => {
+  it('если товар уже добавлен в корзину, повторное нажатие кнопки "добавить в корзину" должно увеличивает его количество', async ({browser}) => {
+      await browser.url('/hw/store/catalog/0' + bug);
+      const addButton = await browser.$('.ProductDetails-AddToCart');
+
+      await addButton.click();
+      await addButton.click();
+
+      await browser.url('/hw/store/cart' + bug);
+
+      const productCount= await browser.$('.Cart-Count');
+      const productCountText = await productCount.getText();
+
+      expect(productCountText).toEqual('2');
+  })
+})
